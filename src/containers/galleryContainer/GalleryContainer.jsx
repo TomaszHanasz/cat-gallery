d976ffa81d5638ec5227eefdbb4ff7650b682064
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-06-02 13:02:31 -0500

    add images on page load

--- src/containers/galleryContainer/GalleryContainer.jsx
+++ src/containers/galleryContainer/GalleryContainer.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SingleImage from "../../components/singleImage/SingleImage";
 import "./galleryContainer.style.css";
 
 const GalleryContainer = () => {
   const [galleryImage, setGalleryImage] = useState([]);
 
-  const onClickHandler = async () => {
+  useEffect(() => {
+    if (galleryImage.length <= 7) {
+      getCatImage();
+    }
+  }, [galleryImage.length]);
+
+  const getCatImage = async () => {
     try {
       const response = await fetch(
         "https://api.thecatapi.com/v1/images/search"
       );
       const data = await response.json();
-      console.log(data);
       setGalleryImage([...galleryImage, data[0].url]);
     } catch (error) {
       console.log(error);
@@ -22,7 +27,7 @@ const GalleryContainer = () => {
     <div>
       <div className="top">
         <h1 className="title">Cat Gallery</h1>
-        <button onClick={onClickHandler} className="cat-btn">
+        <button onClick={getCatImage} className="cat-btn">
           Get random Image
         </button>
       </div>
@@ -31,6 +36,9 @@ const GalleryContainer = () => {
           .map((el, index) => <SingleImage key={index} url={el} />)
           .reverse()}
       </div>
+      <p style={{ textAlign: "center" }}>
+        Copyrights Tomasz Hanasz {new Date().getFullYear()}
+      </p>
     </div>
   );
 };
